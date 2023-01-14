3140369389bc4c858467184ca693d6d73691e16b
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-14 14:43:57 -0700

    division working

--- calc.js
+++ calc.js
@@ -23,6 +23,9 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "*"){
 		console.log(num1 * num2);
 	}
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
 
  
 	// This line closes the connection to the command line interface.

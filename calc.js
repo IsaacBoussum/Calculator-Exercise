71c2f2ac0bb401af24e03bc332b06cbd3c8fc47c
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-14 14:29:34 -0700

    addition working

--- calc.js
+++ calc.js
@@ -14,7 +14,11 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	 }
 
+ 
 	// This line closes the connection to the command line interface.
 	reader.close()
 

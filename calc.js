fe2d5a1c9fc821a88f552d0721b00f757b2ef394
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-14 14:53:21 -0700

    square root working

--- calc.js
+++ calc.js
@@ -26,6 +26,12 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "/"){
 		console.log(num1 / num2);
 	}
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}
+	
+	
+	
 
  
 	// This line closes the connection to the command line interface.
